0dcfa2a95f7758d07640a5135f3246aba469a768
Lettie19 <57149975+Lettie19@users.noreply.github.com>
2021-03-13 12:05:58 -0500

    19.1
    Variables and JQuery

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,21 @@
 // Create a variable below
 
+var helloWorld ='Hello this is Me, Welcome to the Next Level';
+var helloPeople ="Hello people"
 // Console.log() your variable here.
+
+console.log(helloWorld);
+
+var x = '3';
+var y = '2';
+var z = 'There are' + x + y + 'people outside';
+
+function addkids () {
+ console.log(z); 
+ console.log(x);
+ console.log(y);
+ console. ('I am here')
+}
+addkids();
+// The code above runs the function addkids. It is calling the function "function addkids() so you need to add it"
+
