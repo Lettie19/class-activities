692bb2ecc17a1ebcac389f53235dac415b08058f
Lettie19 <57149975+Lettie19@users.noreply.github.com>
2021-03-15 20:40:35 -0400

    19.2

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,22 @@
 $('.tab').on("click", function(){
-  
+
+    
+    if ($(this).hasClass("tab1")) {
+        $(".tab1").addClass("active");
+        $(".tab2").removeClass("active");
+        $("#tabcontent1").css("display", "block");
+        $("#tabcontent2").css("display", "none");
+        console.log("tab 1 was clicked")
+    }
+    }
+
+    else if ($(this).hasClass("tab2")) {
+        $(".tab2").addClass("active");
+        $(".tab1").removeClass("active");
+        $("#tabcontent2").css("display", "block");
+        $("#tabcontent1").css("display", "none");
+        console.log("tab 2 was clicked")
+    }
 });
+
+
